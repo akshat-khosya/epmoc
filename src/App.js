5955ea9dbd2fc65bc0e79265e78fbafa1a4892c3
akshat-khosya <20106@iiitu.ac.in>
2022-01-05 10:35:58 +0530

    startup files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import axios from "axios";
+import Home from './pages/home/Home';
 function App() {
+  const axiosInstance = axios.create({
+		baseURL: "http://localhost:4000/",
+	});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home axiosInstance={axiosInstance} />}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+function Home() {
+    return (
+        <div className='home'>
+            <Navbar />
+            home
+        </div>
+    )
+}
+
+export default Home
