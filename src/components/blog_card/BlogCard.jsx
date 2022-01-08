8eb76d15c9418614ba4fc2f09ce162b5dd8897eb
Nikhil Bhatia <20225@iiitu.ac.in>
2022-01-08 20:55:06 +0530

    Test css

---
+++ src/components/blog_card/BlogCard.jsx
@@ -0,0 +1,13 @@
+import "./blog_card.css";
+function BlogCard(){
+  const blog=5;
+  return(
+  <div>
+  {
+    blog
+  }
+  </div>
+  )
+}
+
+export default BlogCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
-
+import BlogCard from '../../components/blog_card/BlogCard.jsx'
 function Home() {
     return (
         <div className='home'>
             <Navbar />
             home
+            <BlogCard/>
+
         </div>
     )
 }
