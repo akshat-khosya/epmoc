5158df4b083e779b64d5f5e1a747b15190a6cbb9
akshat-khosya <20106@iiitu.ac.in>
2022-01-09 12:38:09 +0530

    added basic html

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import './navbar.css'
-
+import logo from '../../images/epmoc.svg'
 function Navbar() {
     return (
         <div className='navbar'>
-            navbar
+           <div className="navbarcontainer">
+            <div className="navbar-logo">
+                <img src={logo} alt="" />
+                <span>EPMOC</span>
+            </div>
+            <div className="navbar-navigation">
+                <ul className="navbar-items">
+                    <li className="navbar-item navbar-item-active">Home</li>
+                    <li className="navbar-item">Event</li>
+                    <li className="navbar-item">Team</li>
+                    <li className="navbar-item">About</li>
+                    <li className="navbar-item">Calender</li>
+                    <li className="navbar-item">Blog</li>
+                    <li className="navbar-item">Contact Us</li>
+                    <li className="navbar-item">Theme</li>
+                </ul>
+            </div>
+           </div>
         </div>
     )
 }
