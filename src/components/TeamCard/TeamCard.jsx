2dc38e4ad8f03b6c0c9a06e5b2f0f11902c947af
akshat-khosya <akshatdps12@gmail.com>
2022-01-31 12:45:56 +0530

    completed team except core memebers

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -9,13 +9,13 @@ function TeamCard() {
         <div class="card-back">
             <h2>Jane Doe<br /><span>Senior Designer</span></h2>
             <div class="social-icons">
-                <a href="#" class="fab fa-facebook" aria-hidden="true"></a>
-                <a href="#" class="fa fa-twitter" aria-hidden="true"></a>
-                <a href="#" class="fa fa-google-plus" aria-hidden="true"></a>
+                <a href="#" class="far fa-envelope" aria-hidden="true"></a>
+               
                 <a href="#" class="fa fa-linkedin" aria-hidden="true"></a>
                 <a href="#" class="fa fa-instagram" aria-hidden="true"></a>
             </div>
         </div>
+        
     </div>
   );
 }
