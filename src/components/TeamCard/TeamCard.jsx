0aceec2a938304ea1cc8c283f91ac3b55e3be7f3
akshat-khosya <akshatdps12@gmail.com>
2022-01-31 10:53:17 +0530

    added team card

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -3,34 +3,17 @@ import React from "react";
 function TeamCard() {
   return (
     <div class="card">
-      <div class="card__side card__side--front-1">
-        <div class="card__title card__title--1">
-          <i class="fas fa-paper-plane"></i>
-          <h4 class="card__heading">Basic</h4>
+        <div class="card-front"></div>
+        <div class="card-back">
+            <h2>Jane Doe<br /><span>Senior Designer</span></h2>
+            <div class="social-icons">
+                <a href="#" class="fa fa-facebook" aria-hidden="true"></a>
+                <a href="#" class="fa fa-twitter" aria-hidden="true"></a>
+                <a href="#" class="fa fa-google-plus" aria-hidden="true"></a>
+                <a href="#" class="fa fa-linkedin" aria-hidden="true"></a>
+                <a href="#" class="fa fa-instagram" aria-hidden="true"></a>
+            </div>
         </div>
-
-        <div class="card__details">
-          <ul>
-            <li>1 Website</li>
-            <li>50 GB SSD Storage</li>
-            <li>Unmetered Bandwidth</li>
-            <li>Free SSL Certificate</li>
-            <li>1 Included Domain</li>
-            <li>1 Included Domain</li>
-          </ul>
-        </div>
-      </div>
-      <div class="card__side card__side--back card__side--back-1">
-        <div class="card__cta">
-          <div class="card__price-box">
-            <p class="card__price-only">Only</p>
-            <p class="card__price-value">$2.95/mo*</p>
-          </div>
-          <a href="#popup" class="btn btn--white">
-            Select
-          </a>
-        </div>
-      </div>
     </div>
   );
 }
